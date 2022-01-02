313c8673c4896e717c0bf0f3102004585f6d1135
DilshanHarendra <harendracripto@gmail.com>
2022-01-02 23:44:54 +0530

    create default layout

---
+++ simple-news-blog/views/layouts/DefaultLayoutt.swift
@@ -0,0 +1,45 @@
+//
+//  HomeView.swift
+//  simple-news-blog
+//
+//  Created by Dilshan Harendra Perera on 2022-01-02.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+       
+                TabView {
+                    HomeScreenView()
+                        .tabItem {
+                            Image(systemName: "house")
+                            Text("Home")
+                        }
+                    SearchView()
+                        .tabItem {
+                            Image(systemName: "magnifyingglass")
+                            Text("Search")
+                        }
+                    Text("Another Tab")
+                        .tabItem {
+                            Image(systemName: "gearshape")
+                            Text("Setings")
+                        }
+                    Text("The Last Tab")
+                        .tabItem {
+                            Image(systemName: "person")
+                            Text("Profile")
+                        }
+                }
+                .font(.headline)
+       
+        
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
