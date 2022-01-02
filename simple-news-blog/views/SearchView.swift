d7e9a5da36292ff47c11642ba5bcc9f0700055c5
DilshanHarendra <harendracripto@gmail.com>
2022-01-02 23:45:32 +0530

    create sample search page

---
+++ simple-news-blog/views/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  simple-news-blog
+//
+//  Created by Dilshan Harendra Perera on 2022-01-02.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("SearchView")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
